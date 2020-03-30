4819c25288ea9b13916149b1f59fa2465268d949
itacamba <itacamba@live.com>
2020-03-30 16:11:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
+require 'date'
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do #we created a path hello 
+		erb :hello #we are callig our hello.erb file 
+	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 end
